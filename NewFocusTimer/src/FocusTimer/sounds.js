0073ae914bd69447fd6b6880bf55ad54b626eca2
JonatasAS <jonatas23997@live.com>
2023-08-10 22:36:36 -0300

    finished

---
+++ NewFocusTimer/src/FocusTimer/sounds.js
@@ -0,0 +1,7 @@
+export const buttonPressAudio = new Audio('../../assets/button-press.wav')
+
+export const kitchenTimer = new Audio('../../assets/kichen-timer.mp3')
+
+export const bgAudio = new Audio('../../assets/Mass Effect 3 Soundtrack - Leaving Earth (320 kbps).mp3')
+
+bgAudio.loop = true
